c95e915a971f6965e4feb9392a52edd7bd484972
Niksa Janjic <niksajanjic@yahoo.com>
2015-03-24 20:08:57 +0100

    gulp refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 'use strict';
 
-var fs          = require('fs'),
-    gulp        = require('gulp'),
-    concat      = require('gulp-concat'),
-    copy        = require('gulp-copy'),
-    filter      = require('gulp-filter'),
-    gzip        = require('gulp-gzip'),
-    htmlreplace = require('gulp-html-replace'),
-    minifyCSS   = require('gulp-minify-css'),
-    s3          = require('gulp-s3'),
-    sass        = require('gulp-sass'),
-    uglify      = require('gulp-uglify');
+var del          = require('del'),
+    fs           = require('fs'),
+    gulp         = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssmin       = require('gulp-cssmin'),
+    filter       = require('gulp-filter'),
+    gzip         = require('gulp-gzip'),
+    rev          = require('gulp-rev'),
+    revReplace   = require('gulp-rev-replace'),
+    s3           = require('gulp-s3'),
+    sass         = require('gulp-sass'),
+    uglify       = require('gulp-uglify'),
+    useref       = require('gulp-useref'),
+    gutil        = require('gulp-util');
 
 var options = {
   headers: {'Cache-Control': 'max-age=315360000, no-transform, public'},
@@ -19,46 +22,55 @@ var options = {
 
 var aws = JSON.parse(fs.readFileSync('./aws.json'));
 
-gulp.task('copy-images', function(){
+gulp.task('copy-images', ['clean'], function(){
   return gulp.src(['./src/assets/**/*.{png,gif,jpg}'])
-    .pipe(copy('./dist/assets', {prefix: 2}));
+    .pipe(gulp.dest('./dist/assets', {prefix: 2}));
 });
 
-gulp.task('build-html', function(){
-  return gulp.src('./src/index.html')
-    .pipe(htmlreplace({
-      'js': 'js/bundle.min.js',
-      'css': 'styles/main.min.css'
-    }))
+gulp.task('build-html', ['clean', 'styles'], function(){
+  var jsFilter = filter('**/*.js');
+  var cssFilter = filter('**/*.css');
+
+  var userefAssets = useref.assets();
+
+  return gulp.src("./src/index.html")
+    .pipe(userefAssets)             // Concatenate with gulp-useref
+    .pipe(jsFilter)
+    .pipe(uglify())                 // Minify any javascript sources
+    .pipe(jsFilter.restore())
+    .pipe(cssFilter)
+    .pipe(cssmin())                 // Minify any CSS sources
+    .pipe(cssFilter.restore())
+    .pipe(rev())                    // Rename the concatenated files
+    .pipe(userefAssets.restore())
+    .pipe(useref())
+    .pipe(revReplace())             // Substitute in new filenames
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('compress-style', function(){
-  var scssFilter = filter('**/*.scss');
-  return gulp.src(['./src/styles/vendor/**/*.css', './src/styles/**/*.scss'])
-    .pipe(scssFilter)
-    .pipe(sass())
-    .pipe(scssFilter.restore())
-    .pipe(concat('main.min.css'))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('./dist/styles'));
+gulp.task('styles', function() {
+  return gulp.src('./src/styles/**/*.scss')
+    .pipe(sass({
+      onError: function(error) {
+        gutil.log(gutil.colors.red(error));
+        gutil.beep();
+      },
+      onSuccess: function() {
+        gutil.log(gutil.colors.green('Sass styles compiled successfully.'));
+      }
+    }))
+    .pipe(autoprefixer('last 2 versions', 'ie >= 8'))
+    .pipe(gulp.dest('./src/styles'));
 });
 
-gulp.task('compress-js', function(){
-  return gulp.src(['./src/js/jquery.min.js',
-    './src/js/wow.min.js',
-    './src/js/init.js',
-    './src/js/scripts.js' ])
-    .pipe(concat('bundle.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'));
-})
+gulp.task('clean', function(cb) {
+  del(['dist/**'], cb);
+});
 
 gulp.task('build', [
+  'clean',
   'copy-images',
-  'build-html',
-  'compress-style',
-  'compress-js'
+  'build-html'
 ]);
 
 gulp.task('publish', function(){
