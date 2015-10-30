f4167b69ece60265c7ebe6e53e3969f542d193dc
Chris Swenor <chris@ecp.io>
2015-10-30 14:46:21 -0400

    Added process section

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -39,7 +39,7 @@
     // adjusts header height
       $('header').css("height", window.innerHeight);
       $('.cinemagraph').css('height', window.innerHeight);
-      var newHeaderContentHeight = (window.innerHeight / 2) - ($('.header-content').height()/2 + 50); // 50%
+      var newHeaderContentHeight = (window.innerHeight / 2) - ($('.header-content').height()/2); // 50%
       newHeaderContentHeight -= $('.logo').height();
       newHeaderContentHeight = newHeaderContentHeight < 0 ? 0 : newHeaderContentHeight;
       $('.header-content').css("top", newHeaderContentHeight);
